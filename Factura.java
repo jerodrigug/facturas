f1f6548d387fd77d08f0aec5b867125e06a26da7
jerodrigug <28200666+jerodrigug@users.noreply.github.com>
2020-03-31 18:34:48 -0500

    Add files via upload
    qw

---
+++ Factura.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package softwarefactura;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+/**
+ *
+ * @author Alejandro Arroyave
+ */
+public class Factura {
+    private int NroFactura;
+    private LocalDate Fecha;
+    private Cliente Cliente;
+    private int totalCosto = 0;
+    private int Faltaitems;
+    
+    public Factura(int nroFactura, Cliente cliente){
+        NroFactura=nroFactura;
+        //DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        Fecha = LocalDate.now();
+        Cliente=cliente;
+    }
+    
+    public int getNroFactura(){
+        return NroFactura;
+    }
+    
+    public LocalDate getFecha(){
+        return Fecha;
+    }
+    
+    public Cliente getCliente(){
+        return Cliente;
+    }
+    
+    public int getTotalCosto(){
+        return totalCosto;
+    }
+}
---
+++ Item.java
@@ -0,0 +1,38 @@
+package softwarefactura;
+
+/**
+ * Class Item
+ * @author Alejandro Arroyave
+ */
+public class Item {
+    private final int Id;
+    private String Descripcion;
+    //Falta tipo item
+    private int Valor;
+    
+    public Item(int id, String descripcion, int valor){
+        Id=id;
+        Descripcion=descripcion;
+        Valor=valor;
+    }
+    
+    public int getId(){
+        return Id;
+    }
+    
+    public String getDescripcion(){
+        return Descripcion;
+    }
+    
+    public void setDescripcion(String descripcion){
+        Descripcion=descripcion;
+    }
+    
+    public int getValor(){
+        return Valor;
+    }
+    
+    public void setValor(int valor){
+        Valor=valor;
+    }
+}
